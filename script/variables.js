9e176d07f20e149b96c04534d4e8bf973e3ad9c7
Harted <deketelaerejens@gmail.com>
2017-11-22 12:11:10 +0100

    Update variables.js

--- script/variables.js
+++ script/variables.js
@@ -4,6 +4,14 @@ var anim_speed_factor = 1;
 var action_area_corner_offset_factor = 0.1;
 var action_area_base_size_factor = 3/2;
 
+// Colors
+var color_1 = "#424B54";
+var color_2 = "#2CA6A4";
+var color_3 = "#85DCAC";
+var color_4 = "#FFCF00";
+var color_back = "white";
+
+
 // Window width variables
 var window_width, window_height, min_window_size;
 
