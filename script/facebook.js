269f1ca56370ab725760dc4e86f4f7374dfccc58
Harted <deketelaerejens@gmail.com>
2017-11-26 01:41:17 +0100

    Add files via upload

--- script/facebook.js
+++ script/facebook.js
@@ -13,18 +13,33 @@ function GetFacebookEvents(){
       access_token :'131918997473097|22PNWUBJlserYK4cJuiOpwBLCaw',
       limit: '9999'
     },
+
     function(response) {
-      //console.log(response)
+      console.log(response)
       for (n = 0; n < response.data.length; n++){
         date = new Date(response.data[n].start_time)
         console.log(date + " | " + response.data[n].name);
-        events.push("<strong>" + date + " </strong> <br>" + response.data[n].name + "<br><hr>")
+        var dayArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var day = dayArray[date.getDay()]
+        var dayofmonth = date.getDate()
+        var month = monthArray[date.getMonth()]
+        var year = date.getFullYear()
+        if (response.data[n].place.location != undefined){
+          console.log(response.data[n].place.location.city)
+          var city = response.data[n].place.location.city
+        } else {
+          console.log("nergens")
+          var city = ""
+        }
+        var deit = day + ' ' + dayofmonth + ' '+ month + ' ' + year
+        events.push("<strong><h4>" + deit + "</h4> </strong> <br>" + response.data[n].name + "<br><em>" + response.data[n].place.name + "</em><br><h5>" + city + "</h5><br><hr>")
       };
+
       $('#events').html(events).css({
         'opacity': 1,
         'transition': "1000ms",
         'margin-bottom': '50px'
-      })
-    }
-  );
-}
+      });
+    });
+  };
