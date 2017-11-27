094335aa6974fa9ddffa3c503a346083743580c8
Harted <deketelaerejens@gmail.com>
2017-11-27 18:12:31 +0100

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,14 +1,10 @@
-// functions are ordered as to first usage in index.html
-
-// determine if the user is working on a touch or desktop interface -------------------------------------------------------------------
-DetermineTouch()
+// Determine if the user is working on a touch or desktop interface -------------------------------------------------------------------
 function DetermineTouch(){
-	touch = false
-	window.addEventListener('touchstart', function() {
-	  touch = true
-		Squares()
-		console.log(touch)
-		$(document).off("mousemove")
+	touch = false;
+	$(window).on('touchstart', function() {
+	  touch = true;
+		Squares(); //on first touch immediat clicktrough
+		$(this).off('touchstart mousemove');
 	});
 };
 
@@ -57,53 +53,52 @@ function CalculateSizes() {
 	//reference box size and styling
 	if (screen_large == true || screen_medium == true) {
 		ref_box_size = Math.round(min_window_size / (2 + Math.pow(min_window_size / screen_large_size, 3)) * 2);
-		SizesByRefBox(); // main_child
+		SizesByRefBox(); //--------------------------------------------------------- |F| main_child.js
 	} else if (screen_small == true) {
 		ref_box_size = '100%';
-		SizesByRefBoxMobile(); //main_child
+		SizesByRefBoxMobile(); //--------------------------------------------------- |F| main_child.js
 	} else {
 		ref_box_size = Math.round(min_window_size / 3 * 2);
-		SizesByRefBox(); //main_child
+		SizesByRefBox(); //--------------------------------------------------------- |F| main_child.js
 	}
 };
 
 // Reference box ----------------------------------------------------------------------------------------------------------------------
 function MakeRefBox() {
-	$("#reference_box").css({
-		"width": ref_box_size,
-		"height": ref_box_size,
+	$('#reference_box').css({
+		'width': ref_box_size,
+		'height': ref_box_size,
 		//place in absolute center
-		"margin": "auto",
-		"position": "absolute",
-		"top": 0,
-		"left": 0,
-		"bottom": 0,
-		"right": 0,
+		'margin': 'auto',
+		'position': 'absolute',
+		'top': 0,
+		'left': 0,
+		'bottom': 0,
+		'right': 0,
 	})
 };
 
 // Make squares bundle ----------------------------------------------------------------------------------------------------------------
 function Squares() {
 	// (#id | number_counter_clockwise_starting_top_left(1-4) | size(width & height) | href_on click | mouse_enable)
-	MakeSquare("#top_left", 1, box1_size, "about.html"); // main
-	MakeSquare("#bottom_left", 2, box2_size, "music.html"); // main
-	MakeSquare("#bottom_right", 3, box3_size, "video.html"); // main
-	MakeSquare("#top_right", 4, box4_size, "shows.html"); // main
+	MakeSquare('#top_left', 1, box1_size, 'about.html'); //---------------------- |F| main
+	MakeSquare('#bottom_left', 2, box2_size, 'music.html'); //------------------- |F| main
+	MakeSquare('#bottom_right', 3, box3_size, 'video.html'); //------------------ |F| main
+	MakeSquare('#top_right', 4, box4_size, 'shows.html'); //--------------------- |F| main
 };
 
 // Make squares -----------------------------------------------------------------------------------------------------------------------
 function MakeSquare(MS_id, MS_ref, MS_size, MS_href) {
-
 	var MS_margin = hover_size - MS_size;
-	var MS_right, MS_bottom, MS_right_h, MS_bottom_h;
+	var MS_right, MS_bottom, MS_right_h, MS_bottom_h;;
 	var MS_leave_enable = true;
-	var MS_animation_speed = (anim_speed_factor * (hover_size - MS_size) * (800 / ref_box_size)) + "ms";
+	var MS_animation_speed = (anim_speed_factor * (hover_size - MS_size) * (800 / ref_box_size)) + 'ms';
 
-	// set margins
+	// set margins (artwork on desktop and ipad/ fullscreen squares on iphone)
 	if (screen_small == true) {
-		var margins_array = SetMargins(MS_ref, '50%', 0); // main_child
+		var margins_array = SetMargins(MS_ref, '50%', 0); //------------------------ |F| main_child.js
 	} else {
-		var margins_array = SetMargins(MS_ref, hover_size, MS_margin); // main_child
+		var margins_array = SetMargins(MS_ref, hover_size, MS_margin); //----------- |F| main_child.js
 	};
 	var MS_right = margins_array[0];
 	var MS_bottom = margins_array[1];
@@ -117,219 +112,134 @@ function MakeSquare(MS_id, MS_ref, MS_size, MS_href) {
 		'height': MS_size,
 		'right': MS_right,
 		'bottom': MS_bottom,
-		'background-color': eval("color_" + MS_ref),
+		'background-color': eval('color_' + MS_ref),
 		'transition': MS_animation_speed
 	});
 
-	// if small screen or iphone no hover animation and clicktrough
+	// if small screen or iphone no hover animation and clicktrough immediately
 	if (screen_small == true || touch == true) {
-		console.log('touchinterface')
-
-		$(MS_id).off("mouseenter mouseleave click").click(function() {
-			ClickFunction(MS_id, MS_href, eval("color_" + MS_ref))
-			console.log("mobiletap")
+		$(MS_id).off('mouseenter mouseleave click').on('click', function() {
+			ClickFunction(MS_id, MS_href, eval('color_' + MS_ref)) //----------------- |F| main_child.js
 		});
 	} else {
-		console.log('desktop')
 		// MOUSE ENTER
-		$(MS_id).off("click mouseenter mouseleave").on("mouseenter", function() {
-			BoxProximityEnable(MS_ref, false)
+		$(MS_id).off('click mouseenter mouseleave').on('mouseenter', function() {
+			BoxProximityEnable(MS_ref, false) //-------------------------------------- |F| main_child.js
 			$(this).css({
 				'width': hover_size,
 				'height': hover_size,
 				'right': MS_right_h,
 				'bottom': MS_bottom_h,
 			}) //actions on end of transisition:
-			.off('transitionend').one("transitionend", function() {
+			.off('transitionend').one('transitionend', function() {
 				$(this).css({
-					"cursor": "pointer"
-				}).off("click").click(function() {
-					console.log("mouseclick")
-					ClickFunction(MS_id, MS_href)
-					//window.location = MS_href
+					'cursor': 'pointer'
+				}).off('click').on('click', function() {
+					console.log('mouseclick')
+					ClickFunction(MS_id, MS_href) //-------------------------------------- |F| main_child.js
 				})
 			})
 			// logo animation
-			$("#logo").css({
-				"fill": eval("color_" + MS_ref),
+			$('#logo').css({
+				'fill': eval('color_' + MS_ref),
 				'transition': MS_animation_speed
 			})
 		})
 		// MOUSE LEAVE
 		.mouseleave(function() {
-			BoxProximityEnable(MS_ref, true)
-			$(this).off("click").css({
+			BoxProximityEnable(MS_ref, true) //--------------------------------------- |F| main_child.js
+			$(this).off('click').css({
 				'width': MS_size,
 				'height': MS_size,
 				'right': MS_right,
 				'bottom': MS_bottom,
 				'transition': MS_animation_speed,
-				"cursor": "initial",
+				'cursor': 'initial',
 			}).off('transitionend')
 			// logo animation
-			$("#logo").css({
-				"fill": color_back,
+			$('#logo').css({
+				'fill': color_back,
 				'transition': MS_animation_speed
 			});
 		});
 	};
 };
+
 // Make logo --------------------------------------------------------------------------------------------------------------------------
-function MakeLogo() { //hier moet not bottom right bij.. van small naar big transistion heeft ie die nodig
-	if (screen_small != true) {
-		$("#logo").css({
-			"width": logo_size,
-			"height": logo_size,
-			"position": "absolute",
-			"top": logo_ref_center,
-			"left": logo_ref_center,
-			"bottom": logo_ref_center,
-			"right": logo_ref_center,
-			"fill": color_back
-		});
-	} else {
-		$("#logo").css({
-			"width": logo_size,
-			"height": logo_size,
-			"position": "absolute",
-			//place in absolute center
-			"transition": "none",
-			"margin": "auto",
-			"position": "absolute",
-			"top": 0,
-			"left": 0,
-			"bottom": 0,
-			"right": 0,
-			"fill": color_back
-		});
-	}
+function MakeLogo() {
+	$('#logo').css({
+		'position': 'absolute',
+		'width': logo_size,
+		'height': logo_size,
+		//place in absolute center
+		'transition': 'none',
+		'margin': 'auto',
+		'position': 'absolute',
+		'top': 0,
+		'left': 0,
+		'bottom': 0,
+		'right': 0,
+		//color
+		'fill': color_back
+	});
 };
+
 // Box content formatting -------------------------------------------------------------------------------------------------------------
 function BoxContentFormat() {
 	//margin class for box content
-	$('.top_left_margin').css({
-		'margin': box_title_margin + 'px 0px 0px ' + box_title_margin + 'px'
-	});
-	$('.bottom_left_margin').css({
-		'margin': '0px 0px ' + box_title_margin + 'px ' + box_title_margin + 'px'
-	});
-	$('.bottom_right_margin').css({
-		'margin': '0px ' + box_title_margin + 'px ' + box_title_margin + 'px 0px'
-	});
-	$('.top_right_margin').css({
-		'margin': box_title_margin + 'px ' + box_title_margin + 'px 0px 0px'
-	});
+	$('.top_left_margin').css({'margin': box_title_margin + 'px 0px 0px ' + box_title_margin + 'px'});
+	$('.bottom_left_margin').css({'margin': '0px 0px ' + box_title_margin + 'px ' + box_title_margin + 'px'});
+	$('.bottom_right_margin').css({'margin': '0px ' + box_title_margin + 'px ' + box_title_margin + 'px 0px'});
+	$('.top_right_margin').css({'margin': box_title_margin + 'px ' + box_title_margin + 'px 0px 0px'});
 	//box title font size
-	$('h2').css({
-		'font-size': box_title_font_size + 'px'
-	});
+	$('h2').css({'font-size': box_title_font_size + 'px'});
 };
+
 // Ofsets for mouse move interaction --------------------------------------------------------------------------------------------------
 function MouseMoveOfsets() {
-	ref_box_offset_left_center = ($("#reference_box").offset().left + ($("#reference_box").width() / 2));
-	ref_box_offset_top_center = ($("#reference_box").offset().top + ($("#reference_box").height() / 2));
-	box1_offset_left_center = ($("#top_left").offset().left + ($("#top_left").width() / 2));
-	box1_offset_top_center = ($("#top_left").offset().top + ($("#top_left").height() / 2));
-	box2_offset_left_center = ($("#bottom_left").offset().left + ($("#bottom_left").width() / 2));
-	box2_offset_top_center = ($("#bottom_left").offset().top + ($("#bottom_left").height() / 2));
-	box3_offset_left_center = ($("#bottom_right").offset().left + ($("#bottom_right").width() / 2));
-	box3_offset_top_center = ($("#bottom_right").offset().top + ($("#bottom_right").height() / 2));
-	box4_offset_left_center = ($("#top_right").offset().left + ($("#top_right").width() / 2));
-	box4_offset_top_center = ($("#top_right").offset().top + ($("#top_right").height() / 2));
+	ref_box_offset_left_center = ($('#reference_box').offset().left + ($('#reference_box').width() / 2));
+	ref_box_offset_top_center = ($('#reference_box').offset().top + ($('#reference_box').height() / 2));
+	box1_offset_left_center = ($('#top_left').offset().left + ($('#top_left').width() / 2));
+	box1_offset_top_center = ($('#top_left').offset().top + ($('#top_left').height() / 2));
+	box2_offset_left_center = ($('#bottom_left').offset().left + ($('#bottom_left').width() / 2));
+	box2_offset_top_center = ($('#bottom_left').offset().top + ($('#bottom_left').height() / 2));
+	box3_offset_left_center = ($('#bottom_right').offset().left + ($('#bottom_right').width() / 2));
+	box3_offset_top_center = ($('#bottom_right').offset().top + ($('#bottom_right').height() / 2));
+	box4_offset_left_center = ($('#top_right').offset().left + ($('#top_right').width() / 2));
+	box4_offset_top_center = ($('#top_right').offset().top + ($('#top_right').height() / 2));
 	//Action area (Calculate the area the mouse move has effect on the object)
 	box_corner_offset = Math.round(hover_size * action_area_corner_offset_factor);
 	proximity_margin = Math.round(hover_size * action_area_base_size_factor);
 };
 
+// Actions by mouse move --------------------------------------------------------------------------------------------------------------
+function MouseMove() {
+	$(window).off('mousemove').on('mousemove', function() {
+		// Mouse move actions on desktop
+		if (screen_small == false && touch == false) {
+			GetMousePosition(); //---------------------------------------------------- |F| main_child.js
+			//filter double mouse event
+			if (mouse_left != mouse_left_old || mouse_top != mouse_top_old) {
+				var mouse_left_old = mouse_left;
+				varmouse_top_old = mouse_top;
+				// affect squares with proximity data
+				Proximities(); //------------------------------------------------------- |F| main_child.js
+				AffectSquares(); //----------------------------------------------------- |F| main_child.js
+			};
+		};
+	});
+};
+
 // Transition off ---------------------------------------------------------------------------------------------------------------------
 function TransitionOff() {
 	for (n = 0; n < arguments.length; n++) {
-		$(arguments[n]).css("transition", "none");
+		$(arguments[n]).css('transition', 'none');
 	};
 };
 
 // Transition on ----------------------------------------------------------------------------------------------------------------------
 function TransitionOn() {
 	for (n = 0; n < arguments.length; n++) {
-		$(arguments[n]).css("transition", '300ms');
+		$(arguments[n]).css('transition', anim_speed_factor * 300 + 'ms');
 	};
-};
-
-// Actions by mouse move --------------------------------------------------------------------------------------------------------------
-function MouseMove() {
-	$(document).off('mousemove').on('mousemove', function() {
-		// Mouse move actions when cursor
-		if (screen_small == false && touch == false) {
-			GetMousePosition()
-			//filter double mouse event
-			if (mouse_left != mouse_left_old || mouse_top != mouse_top_old) {
-				var mouse_left_old = mouse_left;
-				varmouse_top_old = mouse_top;
-				// affect squares whit proximity data
-				Proximities()
-				AffectSquares()
-			};
-		};
-	});
-};
-
-// Affect squares bundle ----------------------------------------------------------------------------------------------------------------
-function AffectSquares() {
-	AffectSquare("#top_left", 1, box1_proximity_size);
-	AffectSquare("#bottom_left", 2, box2_proximity_size);
-	AffectSquare("#bottom_right", 3, box3_proximity_size);
-	AffectSquare("#top_right", 4, box4_proximity_size);
-}
-// Affect squares -----------------------------------------------------------------------------------------------------------------------
-function AffectSquare(AS_id, AS_ref, AS_size) {
-	var AS_margin = hover_size - AS_size;
-	var AS_right, AS_bottom;
-	var margins_array = SetMargins(AS_ref, hover_size, AS_margin);
-	AS_right = margins_array[0];
-	AS_bottom = margins_array[1];
-	$(AS_id).css({
-		'width': AS_size,
-		'height': AS_size,
-		'right': AS_right,
-		'bottom': AS_bottom,
-	})
-};
-// Create offset to corner farthest from the center of the reference box ----------------------------------------------------------------
-function Proximities() {
-	box1_proximity = BoxProximity(1, box1_offset_top_center - box_corner_offset, box1_offset_left_center - box_corner_offset, proximity_margin);
-	box2_proximity = BoxProximity(2, box2_offset_top_center + box_corner_offset, box2_offset_left_center - box_corner_offset, proximity_margin);
-	box3_proximity = BoxProximity(3, box3_offset_top_center + box_corner_offset, box3_offset_left_center + box_corner_offset, proximity_margin);
-	box4_proximity = BoxProximity(4, box4_offset_top_center - box_corner_offset, box4_offset_left_center + box_corner_offset, proximity_margin);
-	box1_proximity_size = box1_size + (hover_size / 2 * box1_proximity)
-	box2_proximity_size = box2_size + (hover_size / 2 * box2_proximity)
-	box3_proximity_size = box3_size + (hover_size / 2 * box3_proximity)
-	box4_proximity_size = box4_size + (hover_size / 2 * box4_proximity)
-}
-// Calculate proximities - when cursor is getting closer to the ofsettet point things get funny -----------------------------------------
-function BoxProximity(BP_ref, BP_offset_top_center, BP_offset_left_center, BP_proximity_box_size) {
-	if (eval("BoxProximity_" + BP_ref + "_enable") == true) {
-		//value vert. & hor. from center of window (0: center / >0: out of center)
-		var mouse_vertical_from_center = Math.abs(mouse_top - BP_offset_top_center);
-		var mouse_horizontal_from_center = Math.abs(mouse_left - BP_offset_left_center);
-		var pos_neg_vertical = parseFloat(-(mouse_top - BP_offset_top_center) / mouse_vertical_from_center) || 1;
-		var pos_neg_horizontal = parseFloat(-(mouse_left - BP_offset_left_center) / mouse_horizontal_from_center) || 1;
-		//value to center = half min_window_size, value at border of virtual minimum window size square = 0
-		var to_center_vertical_raw = (mouse_vertical_from_center - (BP_proximity_box_size / 2));
-		var to_center_vertical
-		if (to_center_vertical_raw <= 0) {
-			to_center_vertical = Math.abs(to_center_vertical_raw);
-		} else {
-			to_center_vertical = 0
-		}
-		var to_center_horizontal_raw = (mouse_horizontal_from_center - (BP_proximity_box_size / 2));
-		var to_center_horizontal;
-		if (to_center_horizontal_raw <= 0) {
-			to_center_horizontal = Math.abs(to_center_horizontal_raw);
-		} else {
-			to_center_horizontal = 0;
-		}
-		var to_center_vertical_uni = pos_neg_vertical * Math.round(10000 * (to_center_vertical / (BP_proximity_box_size / 2))) / 10000;
-		var to_center_horizontal_uni = pos_neg_horizontal * Math.round(10000 * (to_center_horizontal / (BP_proximity_box_size / 2))) / 10000;
-		return Math.abs(to_center_vertical_uni * to_center_horizontal_uni)
-	}
 };
--- script/main_child.js
+++ script/main_child.js
@@ -1,28 +1,28 @@
 // Sizes by reference box
 function SizesByRefBox() {
-	box1_size = Math.round(ref_box_size * 0.36); //220;
-	box2_size = Math.round(ref_box_size * 0.3); //180;
-	box3_size = Math.round(ref_box_size * 0.24); //140;
-	box4_size = Math.round(ref_box_size * 0.18); //100;
-	hover_size = Math.round(ref_box_size / 2); // Make hover_size an argument in MakeSquare.. only local variables in functions for easy editing!!!!
-	box_title_margin = Math.round(ref_box_size * 0.041); //25;
-	box_title_font_size = Math.round(ref_box_size * 0.033); //20;
+	box1_size = /*Math.round*/(ref_box_size * 0.36); //220;
+	box2_size = /*Math.round*/(ref_box_size * 0.3); //180;
+	box3_size = /*Math.round*/(ref_box_size * 0.24); //140;
+	box4_size = /*Math.round*/(ref_box_size * 0.18); //100;
+	hover_size = /*Math.round*/(ref_box_size / 2); // Make hover_size an argument in MakeSquare.. only local variables in functions for easy editing!!!!
+	box_title_margin = /*Math.round*/(ref_box_size * 0.041); //25;
+	box_title_font_size = /*Math.round*/(ref_box_size * 0.033); //20;
 	//logo sizes
-	logo_size = Math.round(ref_box_size * 0.15); //80;
-	logo_ref_center = Math.round(hover_size - (logo_size / 2));
+	logo_size = /*Math.round*/(ref_box_size * 0.15); //80;
+	logo_ref_center = /*Math.round*/(hover_size - (logo_size / 2));
 };
 
 // Sizes on mobile
 function SizesByRefBoxMobile() {
-	box1_size = "50%"; //220;
-	box2_size = "50%"; //180;
-	box3_size = "50%"; //140;
-	box4_size = "50%"; //100;
-	hover_size = Math.round(min_window_size / 2); // Make hover_size an argument in MakeSquare.. only local variables in functions for easy editing!!!!
-	box_title_margin = Math.round(min_window_size * 0.08); //25;
-	box_title_font_size = Math.round(min_window_size * 0.08); //20;
+	box1_size = '50%'; //220;
+	box2_size = '50%'; //180;
+	box3_size = '50%'; //140;
+	box4_size = '50%'; //100;
+	hover_size = /*Math.round*/(min_window_size / 2); // Make hover_size an argument in MakeSquare.. only local variables in functions for easy editing!!!!
+	box_title_margin = /*Math.round*/(min_window_size * 0.08); //25;
+	box_title_font_size = /*Math.round*/(min_window_size * 0.08); //20;
 	//logo sizes
-	logo_size = Math.round(min_window_size * 0.30); //80;
+	logo_size = /*Math.round*/(min_window_size * 0.30); //80;
 };
 
 // Set margins of squars based on position --------------------------------------------------------------------------------------------
@@ -68,31 +68,32 @@ function BoxProximityEnable(ref, Bool) {
 
 //Function when a box is clicked after transition -------------------------------------------------------------------------------------
 function ClickFunction(CF_id, CF_href, CF_logo_color) {
+	$(window).off('resize mousemove touchstart');
 	var CF_array = [];
-	$("#reference_box > div").each(function() {
-		CF_array[CF_array.length] = "#" + $(this).attr('id');
+	$('#reference_box > div').each(function() {
+		CF_array[CF_array.length] = '#' + $(this).attr('id');
 	});
 	for (n = 0; n < CF_array.length; n++) {
 		if (CF_id != CF_array[n]) {
 			$(CF_array[n]).css({
 				'display': 'none',
-			}).off("mouseenter mouseleave");
+			}).off('mouseenter mouseleave');
 		} else {
-			$(CF_array[n]).off("mouseenter mouseleave transitionend click").css({
+			$(CF_array[n]).off('mouseenter mouseleave transitionend click').css({
 				'cursor': 'initial'
 			});
 		};
 	};
 
-	$("#logo").css({
+	$('#logo').css({
 		//place in absolute center
-		"transition": "none",
-		"margin": 'auto',
-		"position": "absolute",
-		"top": 0,
-		"left": 0,
-		"bottom": 0,
-		"right": 0
+		'transition': 'none',
+		'margin': 'auto',
+		'position': 'absolute',
+		'top': 0,
+		'left': 0,
+		'bottom': 0,
+		'right': 0
 	});
 
 	var margins_to_header = SetMarginsToHeader(CF_id)
@@ -106,32 +107,32 @@ function ClickFunction(CF_id, CF_href, CF_logo_color) {
 		'left': left,
 		'top': top,
 	})
-	$("#reference_box").css({
-		'transition': "none",
+	$('#reference_box').css({
+		'transition': 'none',
 		'width': window_width,
 		'height': window_height,
-		"top": 0,
-		"left": 0,
-		"bottom": 0,
-		"right": 0
-		//'margin': "0px",
-	});
-	$(document).off("mousemove");
-
-	$("#logo").css({
-		"transition": 300 * anim_speed_factor + "ms",
-		"fill": CF_logo_color,
+		'top': 0,
+		'left': 0,
+		'bottom': 0,
+		'right': 0
+		//'margin': '0px',
 	});
 
 
-	$("h2").css({
+	$('#logo').css({
+		'transition': 300 * anim_speed_factor + 'ms',
+		'fill': CF_logo_color,
+	});
+
+
+	$('h2').css({
 		//'display': 'none',
 		'opacity': 0,
-		'transition': 300 * anim_speed_factor + "ms",
+		'transition': 300 * anim_speed_factor + 'ms',
 	});
 	setTimeout(function() {
 		$(CF_id).css({
-			'transition': 300 * anim_speed_factor + "ms",
+			'transition': 300 * anim_speed_factor + 'ms',
 			'width': '100%',
 			'height': header_height,
 			'position':'fixed',
@@ -142,13 +143,13 @@ function ClickFunction(CF_id, CF_href, CF_logo_color) {
 			'left': 0,
 		});
 		setTimeout(function() {
-			$("#logo").css({
-				"transition": 200 * anim_speed_factor + "ms",
-				"width": 0,
-				"height": 0,
+			$('#logo').css({
+				'transition': 200 * anim_speed_factor + 'ms',
+				'width': 0,
+				'height': 0,
 			});
 			setTimeout(function() {
-				window.location = CF_href;
+				//window.location = CF_href;
 			}, 300 * anim_speed_factor)
 		}, 300 * anim_speed_factor)
 	}, 000);
@@ -170,10 +171,71 @@ function SetMarginsToHeader(SMTH_id) {
 
 	console.log(offset_top + ' - ' + offset_left + ' - ' +width + ' - ' +height)
 
-		var top = offset_top + "px";
-		var left = offset_left + "px";
-		var bottom = (window_height - offset_top - height) + "px";
-		var right = (window_width - offset_left - width); + "px"
+		var top = offset_top + 'px';
+		var left = offset_left + 'px';
+		var bottom = (window_height - offset_top - height) + 'px';
+		var right = (window_width - offset_left - width); + 'px'
 
 	return [top, right, bottom, left]
+};
+
+// Affect squares bundle ----------------------------------------------------------------------------------------------------------------
+function AffectSquares() {
+	AffectSquare('#top_left', 1, box1_proximity_size);
+	AffectSquare('#bottom_left', 2, box2_proximity_size);
+	AffectSquare('#bottom_right', 3, box3_proximity_size);
+	AffectSquare('#top_right', 4, box4_proximity_size);
+}
+// Affect squares -----------------------------------------------------------------------------------------------------------------------
+function AffectSquare(AS_id, AS_ref, AS_size) {
+	var AS_margin = hover_size - AS_size;
+	var AS_right, AS_bottom;
+	var margins_array = SetMargins(AS_ref, hover_size, AS_margin);
+	AS_right = margins_array[0];
+	AS_bottom = margins_array[1];
+	$(AS_id).css({
+		'width': AS_size,
+		'height': AS_size,
+		'right': AS_right,
+		'bottom': AS_bottom,
+	})
+};
+// Create offset to corner farthest from the center of the reference box ----------------------------------------------------------------
+function Proximities() {
+	box1_proximity = BoxProximity(1, box1_offset_top_center - box_corner_offset, box1_offset_left_center - box_corner_offset, proximity_margin);
+	box2_proximity = BoxProximity(2, box2_offset_top_center + box_corner_offset, box2_offset_left_center - box_corner_offset, proximity_margin);
+	box3_proximity = BoxProximity(3, box3_offset_top_center + box_corner_offset, box3_offset_left_center + box_corner_offset, proximity_margin);
+	box4_proximity = BoxProximity(4, box4_offset_top_center - box_corner_offset, box4_offset_left_center + box_corner_offset, proximity_margin);
+	box1_proximity_size = box1_size + (hover_size / 2 * box1_proximity)
+	box2_proximity_size = box2_size + (hover_size / 2 * box2_proximity)
+	box3_proximity_size = box3_size + (hover_size / 2 * box3_proximity)
+	box4_proximity_size = box4_size + (hover_size / 2 * box4_proximity)
+}
+// Calculate proximities - when cursor is getting closer to the ofsettet point things get funny -----------------------------------------
+function BoxProximity(BP_ref, BP_offset_top_center, BP_offset_left_center, BP_proximity_box_size) {
+	if (eval('BoxProximity_' + BP_ref + '_enable') == true) {
+		//value vert. & hor. from center of window (0: center / >0: out of center)
+		var mouse_vertical_from_center = Math.abs(mouse_top - BP_offset_top_center);
+		var mouse_horizontal_from_center = Math.abs(mouse_left - BP_offset_left_center);
+		var pos_neg_vertical = parseFloat(-(mouse_top - BP_offset_top_center) / mouse_vertical_from_center) || 1;
+		var pos_neg_horizontal = parseFloat(-(mouse_left - BP_offset_left_center) / mouse_horizontal_from_center) || 1;
+		//value to center = half min_window_size, value at border of virtual minimum window size square = 0
+		var to_center_vertical_raw = (mouse_vertical_from_center - (BP_proximity_box_size / 2));
+		var to_center_vertical
+		if (to_center_vertical_raw <= 0) {
+			to_center_vertical = Math.abs(to_center_vertical_raw);
+		} else {
+			to_center_vertical = 0
+		}
+		var to_center_horizontal_raw = (mouse_horizontal_from_center - (BP_proximity_box_size / 2));
+		var to_center_horizontal;
+		if (to_center_horizontal_raw <= 0) {
+			to_center_horizontal = Math.abs(to_center_horizontal_raw);
+		} else {
+			to_center_horizontal = 0;
+		}
+		var to_center_vertical_uni = pos_neg_vertical * Math.round(10000 * (to_center_vertical / (BP_proximity_box_size / 2))) / 10000;
+		var to_center_horizontal_uni = pos_neg_horizontal * Math.round(10000 * (to_center_horizontal / (BP_proximity_box_size / 2))) / 10000;
+		return Math.abs(to_center_vertical_uni * to_center_horizontal_uni)
+	}
 };
