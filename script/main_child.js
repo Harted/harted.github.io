ad6f4c0d3227acb9088bba524c6cdff47db63e98
Harted <deketelaerejens@gmail.com>
2017-12-01 10:57:24 +0100

    Update main_child.js

--- script/main_child.js
+++ script/main_child.js
@@ -305,20 +305,3 @@ function LinkLogo(LL_id, LL_color, LL_color_dim, LL_href){
 		})
 	})
 };
-// Deep link ----------------------------------------------------------------------------------------------------------------------------
-function DeepLink(web_link, ios_link, android_link) {
-
-	//redirect web
-	setTimeout(function() {
-  		window.location = web_link;
-	}, 20);
-
-	//redirect android
-	setTimeout(function() {
-  		window.location = android_link;
-	}, 10);
-
-	//direct ios
-	window.location = ios_link;
-
-};
