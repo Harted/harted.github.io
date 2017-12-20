320ca39b7c380f636ddb36ce34ca279646cc4be8
Harted <deketelaerejens@gmail.com>
2017-12-20 16:03:09 +0100

    sc track info in html level

--- script/pages_music_ep.js
+++ script/pages_music_ep.js
@@ -191,7 +191,7 @@ function Soundcloud(info, color){
   //console.log(info)
   
   for (key in info) {
-  	console.log(eval(info.key))
+  	console.log(info[key])
   }
 
   $('.play').css('fill', color).attr('width','9')
