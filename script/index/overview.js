de35ab16723ce77843dc191bf6a7cb6614e2717d
Harted (volvo) <deketelaerejens@gmail.com>
2019-10-29 13:21:53 +0100

    shifts

--- script/index/overview.js
+++ script/index/overview.js
@@ -2,6 +2,8 @@ var OVERVIEW
 
 function makeOverview(){
 
+  return; //temp disable
+
   OVERVIEW = {}
 
   for (var i = 0; i < all_alarms.length; i++) {
@@ -90,3 +92,89 @@ function makeOverview(){
   console.log(OVERVIEW)
 
 }
+
+
+
+var SHIFTS = {
+  1: {
+    W: { s: '00:00:00', e: '05:14:59' },
+    S1: { s: '05:15:00', e: '13:29:59' },
+    S2: { s: '13:30:00', e: '21:29:59' },
+    N1: { s: '21:30:00', e: '23:59:59' },
+  },
+  2: {
+    N2: { s: '00:00:00', e: '05:14:59' },
+    S1: { s: '05:15:00', e: '13:29:59' },
+    S2: { s: '13:30:00', e: '21:29:59' },
+    N1: { s: '21:30:00', e: '23:59:59' },
+  },
+  3: {
+    N2: { s: '00:00:00', e: '05:14:59' },
+    S1: { s: '05:15:00', e: '13:29:59' },
+    S2: { s: '13:30:00', e: '21:29:59' },
+    N1: { s: '21:30:00', e: '23:59:59' },
+  },
+  4: {
+    N2: { s: '00:00:00', e: '05:14:59' },
+    S1: { s: '05:15:00', e: '13:29:59' },
+    S2: { s: '13:30:00', e: '21:29:59' },
+    N1:  { s: '21:30:00', e: '23:59:59' },
+  },
+  5: {
+    N2: { s: '00:00:00', e: '05:14:59' },
+    S1: { s: '05:15:00', e: '12:14:59' },
+    S2: { s: '12:15:00', e: '18:59:59' },
+    N1: { s: '19:00:00', e: '23:59:59' },
+  },
+  6: {
+    N2: { s: '00:00:00', e: '03:14:59' },
+    W: { s: '03:15:00', e: '23:59:59' },
+  },
+  7: {
+    W:  { s: '00:00:00', e: '23:59:59' },
+  },
+}
+
+
+function uniT(tStr){
+  arr = tStr.split(':')
+  return new Date(2019,0,1,arr[0], arr[1], arr[2])
+}
+
+
+var today = new Date()
+var today_day = today.getDay()
+var today_t = uniT(today.toTimeString().substr(0,8))
+var tdy_sh =  SHIFTS[today_day]
+
+for (var sh in tdy_sh) {
+  if (tdy_sh.hasOwnProperty(sh)) {
+
+    if (uniT(tdy_sh[sh].s) < today_t && uniT(tdy_sh[sh].e) > today_t ) {
+      console.log(sh)
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
